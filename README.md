93e7a647fc950a34a9d6173654114d908cb939b2
AbilashBodapati <bodapatiabilash98@gmail.com>
2020-06-06 10:06:21 -0400

    Added a template for Sorting Algorithms

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Algorithms
+## Sorting Algorithm
+> 1. Insertion sort
+> 2. Merge Sort
+> 3. Heap Sort
+> 4. Quick Sort
+> 5. Bubble Sort
+> 6. Counting Sort
+> 7. Radix Sort
+> 8. Bucket Sort
+
---
+++ Sorting.py
@@ -0,0 +1,51 @@
+"""
+File Name: Sorting.py
+Author: Abilash Bodapati
+Version: 20200606
+Description:
+    This is a Python module that implements some of the basic Sorting
+    algorithms.
+    This includes:
+        1. Insertion sort
+        2. Merge Sort
+        3. Heap Sort
+        4. Quick Sort
+        5. Bubble Sort
+        6. Counting Sort
+        7. Radix Sort
+        8. Bucket Sort
+    Check out the README.md file for more information regarding the sorting
+    algorithm.
+"""
+
+def insertionSort(self, parameter_list):
+    # TODO: Implement Insertion Sort
+    pass
+
+def mergeSort(self, parameter_list):
+    # TODO: Implement Insertion Sort
+    pass
+
+def heapSort(self, parameter_list):
+    # TODO: Implement Insertion Sort
+    pass
+
+def quickSort(self, parameter_list):
+    # TODO: Implement Insertion Sort
+    pass
+
+def bubbleSort(self, parameter_list):
+    # TODO: Implement Insertion Sort
+    pass
+
+def countingSort(self, parameter_list):
+    # TODO: Implement Insertion Sort
+    pass
+
+def radixSort(self, parameter_list):
+    # TODO: Implement Insertion Sort
+    pass
+
+def bucketSort(self, parameter_list):
+    # TODO: Implement Insertion Sort
+    pass
